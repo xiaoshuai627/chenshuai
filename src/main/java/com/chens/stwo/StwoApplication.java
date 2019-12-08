be1ccf79875b7847d5ee942443e9cbc6fc09ceb7
xiaoshuai627 <627340466@qq.com>
2019-12-08 21:43:54 +0800

    add index

--- src/main/java/com/chens/stwo/StwoApplication.java
+++ src/main/java/com/chens/stwo/StwoApplication.java
@@ -1,7 +1,11 @@
 package com.chens.stwo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import java.util.Arrays;
 
 @SpringBootApplication
 public class StwoApplication {
@@ -9,5 +13,20 @@ public class StwoApplication {
     public static void main(String[] args) {
         SpringApplication.run(StwoApplication.class, args);
     }
+
+    /*
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
+            System.out.println("Let's inspect the beans provided by Spring Boot!");
+
+            String[] beanNames = ctx.getBeanDefinitionNames();
+            Arrays.sort(beanNames);
+
+            for (String beanname:beanNames){
+                System.out.println(beanname);
+            }
+        };
+    }*/
 
 }
---
+++ src/main/java/com/chens/stwo/pojo/User.java
@@ -0,0 +1,45 @@
+package com.chens.stwo.pojo;
+
+import java.util.Date;
+
+public class User {
+
+    private String name;
+    private int age;
+    private char sex;
+
+    public void setName(String name,int age,char sex){
+        this.age = age;
+        this.sex = sex;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setSex(char sex) {
+        this.sex = sex;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public char getSex() {
+        return sex;
+    }
+
+
+    @Override
+    public String toString() {
+        return "["+"name:"+this.name+",age:"+this.age+",sex:"+this.sex+"]";
+    }
+}
