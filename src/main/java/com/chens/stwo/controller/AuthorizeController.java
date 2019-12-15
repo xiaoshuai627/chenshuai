d93089fdd26e6e66e68339308c8f89e37283b146
xiaoshuai627 <627340466@qq.com>
2019-12-15 19:31:21 +0800

    fix bug @Value

--- src/main/java/com/chens/stwo/controller/AuthorizeController.java
+++ src/main/java/com/chens/stwo/controller/AuthorizeController.java
@@ -9,37 +9,49 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class AuthorizeController {
 
     @Autowired
     private GithubProvider githubProvider;
 
-    @Value("${github.client_id}")
+    @Value("${user.id}")
     private String client_id;
 
-    @Value("${github.client_secret}")
+    @Value("${user.client_secret}")
     private String client_secret;
 
-    @Value("${github.redirect_dir}")
-    private String redirect_dir;
+    @Value("${user.redirect_uri}")
+    private String redirect_uri;
 
     @GetMapping("/callback")
-    public String callback(@RequestParam(name="code") String code,
-                           @RequestParam(name="state") String state) {
+    public String callback(@RequestParam(name = "code") String code,
+                           @RequestParam(name = "state") String state,
+                           HttpServletRequest request) {
 
         System.out.println("点击登陆后回跳到callback,AuthorizeController");
+
         AccessToken accessToken = new AccessToken();
         accessToken.setClient_id(client_id);
         accessToken.setClient_secret(client_secret);
         accessToken.setCode(code);
-        accessToken.setRedirect_uri(redirect_dir);
+        accessToken.setRedirect_uri(redirect_uri);
         accessToken.setState(state);
-        System.out.println("打印accessToken："+accessToken);
-        String  access = githubProvider.getAccessToken(accessToken);
+        System.out.println("打印accessToken：" + accessToken);
+
+        String access = githubProvider.getAccessToken(accessToken);
         GitUser user = githubProvider.getUser(access);
-        System.out.println(user.getName());
-        return "index";
+//        System.out.println(user.getName());
+
+        // 登陆用户展示
+        if (user != null) {
+            request.getSession().setAttribute("user", user);
+            return "redirect:/";
+        } else {
+            return "redirect:/";
+        }
     }
 
 
